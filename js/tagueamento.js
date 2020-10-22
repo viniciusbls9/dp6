fa6b6ca8c5cc3235caf71577f5ef79d153082596
Vinicius Benedito <beneditoloura@gmail.com>
2020-10-22 07:06:28 -0300

    DP6

---
+++ js/tagueamento.js
@@ -0,0 +1,31 @@
+// Preencha este arquivo com qualquer código que você necessite para realizar a
+// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
+// Manager. No último caso, não é necessário implementar a tag <noscript>.
+// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
+// para fazer a sua coleta.
+// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
+
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics_debug.js', 'ga');
+
+ga('create', 'UA-12345-6', 'auto');
+ga('send', 'pageview', location.pathname);
+
+// var submit = jQuery('.contato button[type="submit"]');
+// jQuery('.contato').on('submit', function (e) {
+//     submit.attr('disabled', 'disabled');
+//     e.preventDefault();
+//     setTimeout(function () {
+//         ga('send', {
+//             hitType: 'event',
+//             eventCategory: 'contato',
+//             eventAction: 'enviado',
+//             eventLabel: 'enviado'
+//         });
+//     }, Math.random() * 2000);
+// });
